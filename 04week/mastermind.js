e61994534bf96662e46d30c9651c9842691b8932
Jonathan Rivas <54569323+Rivasjon@users.noreply.github.com>
2019-11-24 22:38:27 -0600

    Update mastermind.js

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -3,8 +3,8 @@
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+input: process.stdin,
+output: process.stdout
 });
 
 let board = [];
@@ -12,97 +12,119 @@ let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
 function printBoard() {
-  for (let i = 0; i < board.length; i++) {
-    console.log(board[i]);
-  }
+for (let i = 0; i < board.length; i++) {
+console.log(board[i]);
+}
 }
 
 function generateSolution() {
-  for (let i = 0; i < 4; i++) {
-    const randomIndex = getRandomInt(0, letters.length);
-    solution += letters[randomIndex];
-  }
+for (let i = 0; i < 4; i++) {
+const randomIndex = getRandomInt(0, letters.length);
+solution += letters[randomIndex];
+}
 }
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function generateHint(guess) {
-  // your code here
-  const solutionArray = solution.split('');
-  // should be ['null', 'null', 'null', 'd']
+const solutionArray = solution.split('');
+// should be [ 'null', 'null', 'null', 'd' ]
 
-  const guessArray = guess.split('');
-  // if guess was 'abdc'
-  // should be [ 'a', 'b', 'd', 'c' ]
- 
-  let redPegs = 0;
-  let whitePegs = 0;
-  
-  //check for redPegs
-  for (let i = 0; i < solutionArray.length; i++) {
-    if (solutionArray[i] === guessArray[i]) {
-      redPegs++;
-      solutionArray[i] = null;
-    }
-  }
-  
+const guessArray = guess.split('');
+// if guess was 'abdc'
+// should be [ 'a', 'b', 'd', 'c' ]
+
+let redPegs = 0;
+let whitePegs = 0;
+
+// checking for redPegs
+for (let i = 0; i < solutionArray.length; i++) {
+if (solutionArray[i] === guessArray[i]) {
+redPegs++;
+solutionArray[i] = null;
+}
+}
+
 let targetIndex = null;
 
-// check for whitePegs
 for (let i = 0; i < guessArray.length; i++) {
-    targetIndex = solutionArray.indexOf(guessArray[i]);
+targetIndex = solutionArray.indexOf(guessArray[i]);
 
-    if (targetIndex > -1) {
-      whitePegs++;
-      solutionArray[i] = null;
-    }
-  }
+if (targetIndex > -1) {
+whitePegs++;
+solutionArray[targetIndex] = null;
+}
+}
 
 
-// return a string representation of redPegs and whitePegs variables
+return $redPegs}-${whitePegs};
+
+
+function addguesshint(guess) {
+let hint = generateHint(guess);
+board.push(hint)
+}
+
 function mastermind(guess) {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
-  
+
+if (guess.length === 4 && guess !== solution ) {
+generateHint(guess)
+
+}
+else if(guess === solution) {
+console.log('You guessed it!')
+return 'You guessed it!'
+
+}
+if(guess > board.length) {
+console.log('game over' + solution)
+}
+else if(guess.length < 4) {
+console.log('guess must be 4 letters')
+}
+else {
+addguesshint(guess)
+console.log('guess again')
+}
 }
 
 
 function getPrompt() {
-  rl.question('guess: ', (guess) => {
-    mastermind(guess);
-    printBoard();
-    getPrompt();
-  });
-}
+rl.question('guess: ', (guess) => {
+mastermind(guess);
+printBoard();
+getPrompt();
+});
+}``
 
 // Tests
 
 if (typeof describe === 'function') {
-  solution = 'abcd';
-  describe('#mastermind()', () => {
-    it('should register a guess and generate hints', () => {
-      mastermind('aabb');
-      assert.equal(board.length, 1);
-    });
-    it('should be able to detect a win', () => {
-      assert.equal(mastermind(solution), 'You guessed it!');
-    });
-  });
+solution = 'abcd';
+describe('#mastermind()', () => {
+it('should register a guess and generate hints', () => {
+mastermind('aabb');
+assert.equal(board.length, 1);
+});
+it('should be able to detect a win', () => {
+assert.equal(mastermind(solution), 'You guessed it!');
+});
+});
 
-  describe('#generateHint()', () => {
-    it('should generate hints', () => {
-      assert.equal(generateHint('abdc'), '2-2');
-    });
-    it('should generate hints if solution has duplicates', () => {
-      assert.equal(generateHint('aabb'), '1-1');
-    });
+describe('#generateHint()', () => {
+it('should generate hints', () => {
+assert.equal(generateHint('abdc'), '2-2');
+});
+it('should generate hints if solution has duplicates', () => {
+assert.equal(generateHint('aabb'), '1-1');
+});
 
-  });
+});
 
 } else {
 
-  generateSolution();
-  getPrompt();
+generateSolution();
+getPrompt();
 }
